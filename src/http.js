f931bfae7f95e54d9f5c6dce9e2d52934bc750bc
Mahmoud Alsaqqa <alsfeer.m1992m@gmail.com>
2026-03-05 11:43:08 +0200

    Sending Data with POST Requests

--- src/http.js
+++ src/http.js
@@ -6,3 +6,17 @@ export async function fetchAvailablePlaces() {
   }
   return resDate.places;
 }
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ places: places }),
+  });
+  const resDate = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to update user places.");
+  }
+  return resDate.message;
+}
